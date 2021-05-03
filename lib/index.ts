9c07af41f06ae2c0f0e1adf5682beadfb0005cbb
PedroHLC <root@pedrohlc.com>
2021-05-03 12:27:51 -0300

    Run prettier

--- lib/index.ts
+++ lib/index.ts
@@ -9,252 +9,272 @@ let destPath = path.join(projectPath, '.elm-i18n');
 let moduleNamespace = 'I18n';
 let destNamespacePath = path.join(destPath, moduleNamespace);
 const configJson = path.join(projectPath, 'i18n.json');
-type Config = Partial<{ source: string, dest: string, namespace: string }>;
+type Config = Partial<{ source: string; dest: string; namespace: string }>;
 
-export function main (): void {
-    if (fs.existsSync(configJson)) {
-        const rawJSON = fs.readFileSync(configJson);
-        const json: Config = JSON.parse(rawJSON.toString());
-        if (json.source != undefined)
-            sourcePath = path.join(projectPath, json.source);
-        if (json.dest != undefined)
-            destPath = path.join(projectPath, json.dest);
-        if (json.namespace != undefined)
-            moduleNamespace = json.namespace;
-        destNamespacePath = path.join(destPath, ...moduleNamespace.split('.'));
+export function main(): void {
+  if (fs.existsSync(configJson)) {
+    const rawJSON = fs.readFileSync(configJson);
+    const json: Config = JSON.parse(rawJSON.toString());
+    if (json.source != undefined)
+      sourcePath = path.join(projectPath, json.source);
+    if (json.dest != undefined) destPath = path.join(projectPath, json.dest);
+    if (json.namespace != undefined) moduleNamespace = json.namespace;
+    destNamespacePath = path.join(destPath, ...moduleNamespace.split('.'));
+  }
+
+  if (!fs.existsSync(destPath)) fs.mkdirSync(destPath);
+  if (!fs.existsSync(destNamespacePath)) fs.mkdirSync(destNamespacePath);
+
+  fs.readdir(sourcePath, function (err, files) {
+    if (err) {
+      return console.log('Unable to scan directory: ' + err);
     }
 
-    if (!fs.existsSync(destPath))
-        fs.mkdirSync(destPath);
-    if (!fs.existsSync(destNamespacePath))
-        fs.mkdirSync(destNamespacePath);
+    let typed: boolean | null = null;
 
-    fs.readdir(sourcePath, function (err, files) {
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        }
+    files.forEach(function (fileName) {
+      if (fileName.startsWith('.')) return;
+      if (!fileName.endsWith('.json')) {
+        console.log('Ignoring "' + fileName + '".');
+        return;
+      }
 
-        let typed: boolean | null = null;
-
-        files.forEach(function (fileName) {
-            if(fileName.startsWith('.')) return;
-            if(!fileName.endsWith('.json')) {
-                console.log('Ignoring "' + fileName + '".');
-                return;
-            }
-
-            const rawJSON = fs.readFileSync(path.join(sourcePath, fileName));
-            const parsedJSON = JSON.parse(rawJSON.toString().replace(/\\/g, '\\\\'));
-            if (typed === null)
-                typed = buildHelpers(parsedJSON);
-            buildLang(fileName, parsedJSON);
-        });
+      const rawJSON = fs.readFileSync(path.join(sourcePath, fileName));
+      const parsedJSON = JSON.parse(rawJSON.toString().replace(/\\/g, '\\\\'));
+      if (typed === null) typed = buildHelpers(parsedJSON);
+      buildLang(fileName, parsedJSON);
     });
+  });
 }
 
-function die (explanation: string): never {
-    console.log(explanation);
-    return process.exit(1);
+function die(explanation: string): never {
+  console.log(explanation);
+  return process.exit(1);
 }
 
-function buildHelpers (data: JSON): boolean {
-    console.log('Bulding Types.elm / Decoders.elm / MockLanguage.elm');
-    const typesBuffer = pipeToElmFormat(path.join(destNamespacePath, 'Types.elm'));
-    const decodersBuffer = pipeToElmFormat(path.join(destNamespacePath, 'Decoders.elm'));
-    const mockBuffer = pipeToElmFormat(path.join(destNamespacePath, 'MockLanguage.elm'));
+function buildHelpers(data: JSON): boolean {
+  console.log('Bulding Types.elm / Decoders.elm / MockLanguage.elm');
+  const typesBuffer = pipeToElmFormat(
+    path.join(destNamespacePath, 'Types.elm'),
+  );
+  const decodersBuffer = pipeToElmFormat(
+    path.join(destNamespacePath, 'Decoders.elm'),
+  );
+  const mockBuffer = pipeToElmFormat(
+    path.join(destNamespacePath, 'MockLanguage.elm'),
+  );
 
-    typesBuffer.write(`module ${moduleNamespace}.Types exposing (..)\n`);
-    decodersBuffer.write(`module ${moduleNamespace}.Decoders exposing (..)\n`
-        +`import ${moduleNamespace}.Types as Types\n`
-        +'import Json.Decode as Decode exposing (Decoder)\n'
-        +'\n'
-        +decodersHelpers
-        );
-    mockBuffer.write(`module ${moduleNamespace}.MockLanguage exposing (..)\n`
-        +`import ${moduleNamespace}.Types as Types\n`
-        );
+  typesBuffer.write(`module ${moduleNamespace}.Types exposing (..)\n`);
+  decodersBuffer.write(
+    `module ${moduleNamespace}.Decoders exposing (..)\n` +
+      `import ${moduleNamespace}.Types as Types\n` +
+      'import Json.Decode as Decode exposing (Decoder)\n' +
+      '\n' +
+      decodersHelpers,
+  );
+  mockBuffer.write(
+    `module ${moduleNamespace}.MockLanguage exposing (..)\n` +
+      `import ${moduleNamespace}.Types as Types\n`,
+  );
 
-    addHelper({ name: '', context: '', data, typesBuffer, decodersBuffer, mockBuffer});
+  addHelper({
+    name: '',
+    context: '',
+    data,
+    typesBuffer,
+    decodersBuffer,
+    mockBuffer,
+  });
 
-    typesBuffer.end();
-    decodersBuffer.end();
-    mockBuffer.end();
-    return true;
+  typesBuffer.end();
+  decodersBuffer.end();
+  mockBuffer.end();
+  return true;
 }
 
 const subEntryRegex = /(?<={{)([^}]+)(?=}})/g;
 const subEntrySed = /{{([^}]+)}}/g;
 
 type AddHelperAccumulator = {
-    name: string,
-    context: string,
-    data: JSON,
-    typesBuffer: Writable,
-    decodersBuffer: Writable,
-    mockBuffer: Writable
+  name: string;
+  context: string;
+  data: JSON;
+  typesBuffer: Writable;
+  decodersBuffer: Writable;
+  mockBuffer: Writable;
 };
 
 function addHelper(accumulator: AddHelperAccumulator): void {
-    const {data, typesBuffer, decodersBuffer, mockBuffer} = accumulator;
-    let {name, context} = accumulator;
+  const { data, typesBuffer, decodersBuffer, mockBuffer } = accumulator;
+  let { name, context } = accumulator;
 
-    const record: string[] = [];
-    const decoder: string[] = [];
-    const mock: string[] = [];
+  const record: string[] = [];
+  const decoder: string[] = [];
+  const mock: string[] = [];
 
-    Object.entries(data).forEach(([key, value]) => {
-        const fieldKey = asFieldName(key);
-        const newContext = (context == '' ? key : context + '.' + key);
+  Object.entries(data).forEach(([key, value]) => {
+    const fieldKey = asFieldName(key);
+    const newContext = context == '' ? key : context + '.' + key;
 
-        if (Array.isArray(value)) {
-            die('Unexpected array in JSON');
-        } else if (typeof value == 'string') {
-            const subEntries = value.match(subEntryRegex);
-            if (subEntries == null) {
-                record.push(fieldKey + " : String");
-                decoder.push(`i18nField "${key}" fallback.${fieldKey}`);
-                mock.push(`${fieldKey} = "${newContext}"`);
-            } else {
-                record.push(
-                    fieldKey
-                    + " : { "
-                    + subEntries.map((v) => asFieldName(v) + ' : String').join(", ")
-                    + " } -> String"
-                );
-                decoder.push(
-                    `i18nReplaceable "${key}" (\\value {`
-                    + subEntries.map((v) => asFieldName(v)).join(', ')
-                    + '} -> translator ['
-                    + subEntries.map((v) => `( "${v}", ${asFieldName(v)} )`).join('\n , ')
-                    + `] value ) fallback.${fieldKey}`
-                );
-                mock.push(`${fieldKey} = always "${newContext}"`);
-            }
-        } else if (value !== null && typeof value == 'object') {
-            const newRecord = (name + capitalize(key));
-            const newFunction = asFieldName(newRecord);
-            record.push(fieldKey + " : " + newRecord);
-            decoder.push(`i18nRecord "${key}" (${newFunction} translator) fallback.${asFieldName(key)}`);
-            mock.push(`${fieldKey} = ${newFunction}`);
-            addHelper({name: newRecord, context: newContext, data: value, typesBuffer, decodersBuffer, mockBuffer});
-        } else
-            die('Invalid JSON');
-    });
+    if (Array.isArray(value)) {
+      die('Unexpected array in JSON');
+    } else if (typeof value == 'string') {
+      const subEntries = value.match(subEntryRegex);
+      if (subEntries == null) {
+        record.push(fieldKey + ' : String');
+        decoder.push(`i18nField "${key}" fallback.${fieldKey}`);
+        mock.push(`${fieldKey} = "${newContext}"`);
+      } else {
+        record.push(
+          fieldKey +
+            ' : { ' +
+            subEntries.map((v) => asFieldName(v) + ' : String').join(', ') +
+            ' } -> String',
+        );
+        decoder.push(
+          `i18nReplaceable "${key}" (\\value {` +
+            subEntries.map((v) => asFieldName(v)).join(', ') +
+            '} -> translator [' +
+            subEntries
+              .map((v) => `( "${v}", ${asFieldName(v)} )`)
+              .join('\n , ') +
+            `] value ) fallback.${fieldKey}`,
+        );
+        mock.push(`${fieldKey} = always "${newContext}"`);
+      }
+    } else if (value !== null && typeof value == 'object') {
+      const newRecord = name + capitalize(key);
+      const newFunction = asFieldName(newRecord);
+      record.push(fieldKey + ' : ' + newRecord);
+      decoder.push(
+        `i18nRecord "${key}" (${newFunction} translator) fallback.${asFieldName(
+          key,
+        )}`,
+      );
+      mock.push(`${fieldKey} = ${newFunction}`);
+      addHelper({
+        name: newRecord,
+        context: newContext,
+        data: value,
+        typesBuffer,
+        decodersBuffer,
+        mockBuffer,
+      });
+    } else die('Invalid JSON');
+  });
 
-    if (name == '')
-        name = 'Root';
+  if (name == '') name = 'Root';
 
-    typesBuffer.write('type alias ' + name + ' =\n    { ');
-    typesBuffer.write(record.join('\n    , '));
-    typesBuffer.write('\n    }\n\n\n');
+  typesBuffer.write('type alias ' + name + ' =\n    { ');
+  typesBuffer.write(record.join('\n    , '));
+  typesBuffer.write('\n    }\n\n\n');
 
-    if (context == '')
-        context = 'root';
-    else
-        context = asFieldName(name);
+  if (context == '') context = 'root';
+  else context = asFieldName(name);
 
-    decodersBuffer.write(`${context} : I18nTranslator -> Types.${name} -> Decoder Types.${name}\n`
-        +`${context} translator fallback =\n`
-        +`    Decode.succeed Types.${name}\n`);
-    if (decoder.length > 0) {
-        decodersBuffer.write('\n    |> ');
-        decodersBuffer.write(decoder.join('\n    |> '));
-    }
-    decodersBuffer.write('\n');
+  decodersBuffer.write(
+    `${context} : I18nTranslator -> Types.${name} -> Decoder Types.${name}\n` +
+      `${context} translator fallback =\n` +
+      `    Decode.succeed Types.${name}\n`,
+  );
+  if (decoder.length > 0) {
+    decodersBuffer.write('\n    |> ');
+    decodersBuffer.write(decoder.join('\n    |> '));
+  }
+  decodersBuffer.write('\n');
 
-    mockBuffer.write(`${context} : Types.${name}\n`
-        +`${context} =\n    { `);
-    mockBuffer.write(mock.join('\n    , '));
-    mockBuffer.write('\n    }\n\n\n');
+  mockBuffer.write(`${context} : Types.${name}\n` + `${context} =\n    { `);
+  mockBuffer.write(mock.join('\n    , '));
+  mockBuffer.write('\n    }\n\n\n');
 }
 
-function buildLang (sourceFileName: string, data: JSON): boolean {
-    const moduleName = capitalize(path.basename(sourceFileName, '.json'));
-    const fileName = moduleName+'.elm';
-    console.log('Building ' + fileName);
-    const filePath = path.join(destNamespacePath, fileName);
-    const buffer = pipeToElmFormat(filePath);
+function buildLang(sourceFileName: string, data: JSON): boolean {
+  const moduleName = capitalize(path.basename(sourceFileName, '.json'));
+  const fileName = moduleName + '.elm';
+  console.log('Building ' + fileName);
+  const filePath = path.join(destNamespacePath, fileName);
+  const buffer = pipeToElmFormat(filePath);
 
-    buffer.write(`module ${moduleNamespace}.${moduleName} exposing (..)\n`
-        +`import ${moduleNamespace}.Types exposing (..)\n`)
+  buffer.write(
+    `module ${moduleNamespace}.${moduleName} exposing (..)\n` +
+      `import ${moduleNamespace}.Types exposing (..)\n`,
+  );
 
-    addValue({name: '', data, buffer});
+  addValue({ name: '', data, buffer });
 
-    buffer.end();
-    return true;
+  buffer.end();
+  return true;
 }
 
 type AddValueAccumulator = {
-    name: string,
-    data: JSON,
-    buffer: Writable
+  name: string;
+  data: JSON;
+  buffer: Writable;
 };
 
 function addValue(accumulator: AddValueAccumulator): void {
-    const {buffer, data} = accumulator;
-    let {name} = accumulator;
+  const { buffer, data } = accumulator;
+  let { name } = accumulator;
 
-    const record: string[] = [];
+  const record: string[] = [];
 
-    Object.entries(data).forEach(([key, value]) => {
-        const fieldKey = asFieldName(key);
+  Object.entries(data).forEach(([key, value]) => {
+    const fieldKey = asFieldName(key);
 
-        if (Array.isArray(value)) {
-            die('Unexpected array in JSON');
-        } else if (typeof value == 'string') {
-            const subEntries = value.match(subEntryRegex);
-            if (subEntries == null)
-                record.push(fieldKey + " = \"" + value + "\"");
-            else
-                record.push(fieldKey
-                    + " = \\{ "
-                    + subEntries.map((v) => asFieldName(v)).join(", ")
-                    + " } -> \""
-                    + value.replace(subEntrySed, "\" ++ $1 ++ \"")
-                    + "\""
-                );
-        } else if (value !== null && typeof value == 'object') {
-            const newRecord = (name + capitalize(key))
-            record.push(fieldKey + " = " + asFieldName(newRecord));
-            addValue({name: newRecord, data: value, buffer});
-        } else
-            die('Invalid JSON');
-    });
+    if (Array.isArray(value)) {
+      die('Unexpected array in JSON');
+    } else if (typeof value == 'string') {
+      const subEntries = value.match(subEntryRegex);
+      if (subEntries == null) record.push(fieldKey + ' = "' + value + '"');
+      else
+        record.push(
+          fieldKey +
+            ' = \\{ ' +
+            subEntries.map((v) => asFieldName(v)).join(', ') +
+            ' } -> "' +
+            value.replace(subEntrySed, '" ++ $1 ++ "') +
+            '"',
+        );
+    } else if (value !== null && typeof value == 'object') {
+      const newRecord = name + capitalize(key);
+      record.push(fieldKey + ' = ' + asFieldName(newRecord));
+      addValue({ name: newRecord, data: value, buffer });
+    } else die('Invalid JSON');
+  });
 
-    if (name == '')
-        name = 'Root';
+  if (name == '') name = 'Root';
 
-    const fieldName = asFieldName(name);
+  const fieldName = asFieldName(name);
 
-    buffer.write(`${fieldName} : ${name}\n${fieldName} =\n    {\n`);
-    buffer.write(record.join('\n    , '));
-    buffer.write('\n    }\n');
+  buffer.write(`${fieldName} : ${name}\n${fieldName} =\n    {\n`);
+  buffer.write(record.join('\n    , '));
+  buffer.write('\n    }\n');
 }
 
 function pipeToElmFormat(filePath: string): Writable | never {
-    const subprocess = child_process.spawn('elm-format',
-        ['--stdin', '--output', filePath],
-        { stdio: [ 'pipe', 1, 2 ] }
-    );
+  const subprocess = child_process.spawn(
+    'elm-format',
+    ['--stdin', '--output', filePath],
+    { stdio: ['pipe', 1, 2] },
+  );
 
-    if (subprocess.stdin === null) {
-        return die('Unable to pipe to elm-format!');
-    }
+  if (subprocess.stdin === null) {
+    return die('Unable to pipe to elm-format!');
+  }
 
-    return subprocess.stdin;
+  return subprocess.stdin;
 }
 
-function capitalize (s: string): string {
-    return s.charAt(0).toUpperCase() + s.slice(1);
+function capitalize(s: string): string {
+  return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
-function asFieldName (s: string): string {
-    const head = s.charAt(0);
+function asFieldName(s: string): string {
+  const head = s.charAt(0);
 
-    if (head >= '0' && head <= '9')
-        return 'n' + s;
+  if (head >= '0' && head <= '9') return 'n' + s;
 
-    return s.charAt(0).toLowerCase() + s.slice(1)
+  return s.charAt(0).toLowerCase() + s.slice(1);
 }
 
 const decodersHelpers = `
